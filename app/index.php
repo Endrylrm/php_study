17587289688f4283425cf75ada498b0f56403314
Endryl Richard Monteiro <endrylrm@hotmail.com>
2025-04-22 16:00:55 -0300

    fix database.

--- app/index.php
+++ app/index.php
@@ -8,6 +8,7 @@ Router::get("/", function () {
     echo '<h1>Hello World!</h1>';
 
     $dbHandler = new DatabaseHandler();
+    $db = $dbHandler->createDBConnection();
 
     //$dbHandler->createUser(["Teste6", "teste6@hotmail.com", 30]);
     //$dbHandler->createMultipleUsers([["Teste4", "teste4@hotmail.com", 30], ["Teste5", "teste5@hotmail.com", 30], ["Teste6", "teste6@hotmail.com", 30]]);
@@ -18,6 +19,9 @@ Router::get("/", function () {
     foreach ($users as $user) {
         echo $user->name . " " . $user->email . " " . $user->age . "<br>";
     }
+
+    $dbHandler = null;
+    $db = null;
 });
 
 Router::get("/test", function () {
